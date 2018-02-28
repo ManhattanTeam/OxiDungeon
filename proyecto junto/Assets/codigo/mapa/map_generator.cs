72a4e5125be8ea6afc90db0f19a7b12b7cb2273f
miguelangelgil <36223494+miguelangelgil@users.noreply.github.com>
2018-02-28 16:41:51 +0100

    V1.304

--- proyecto junto/Assets/codigo/mapa/map_generator.cs
+++ proyecto junto/Assets/codigo/mapa/map_generator.cs
@@ -3,39 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class room: MonoBehaviour {
-	private GameObject [,] sala;
-    private int dimensionX;
-    private int dimensionY;
-	public void GetSala(GameObject[,]sala,int dimensionX, int dimensionY){
-        Debug.Log("he entrado");
-        this.sala = new GameObject[dimensionX,dimensionY+1];
-		//this.sala = sala;
-        this.dimensionX = dimensionX;
-        this.dimensionY = dimensionY;
-        for(int x = 0;x < this.dimensionX; x++){
-            for(int y = 0; y < this.dimensionY; y++){
-                this.sala[x,y] = sala[x,y];
-                Debug.Log(this.sala[x,y]);
-
-            }
-        }
-
-	}
-    public void DestroySala() {
-        Destroy(sala[1,1]);
-        for (int x = 0; x < dimensionX; x ++)
-        {
-            for (int y = 0; y < dimensionY; y++)
-            {
-                Destroy(sala[x, y]);
-
-            }
-        }
-
-    }
-
-}
 public class map_generator : MonoBehaviour {
 	public GameObject baldosa;
 	public Sprite pared;
@@ -45,8 +12,11 @@ public class map_generator : MonoBehaviour {
 	public GameObject [,] sala;
 	public GameObject jugador;
 	public Camera camara;
+    public GameObject Room;
 	private room [] rooms;
-	int dimensionx = 10;
+    public GameObject[] Rooms;
+    int nsalas;
+	int dimensionx = 30;
 	int dimensiony = 10;
 	int columnas = 0;
 	int filas = 0;
@@ -58,79 +28,93 @@ public class map_generator : MonoBehaviour {
     // Use this for initialization
     void Start()
     {
-        rooms = new room[1];
-        sala = new GameObject[dimensionx, dimensiony + 1];
-        for (; columnas < dimensionx; columnas++)
-        {
-            for (; filas < dimensiony; filas++)
+        //nsalas = UnityEngine.Random.Range(5,9);
+        nsalas = 1;
+        rooms = new room[nsalas];
+        Rooms = new GameObject[nsalas]; 
+        
+        for(int i = 0;i < nsalas;i++){
+            //dimensionx = UnityEngine.Random.Range(9,37);
+            //dimensiony = UnityEngine.Random.Range(9,37);
+            sala = new GameObject[dimensionx, dimensiony + 1];
+            Rooms[0] = Instantiate(Room,new Vector2(auxX,auxY),transform.rotation);
+            rooms[0] = Rooms[0].GetComponent<room>();
+
+        
+            for (; columnas < dimensionx; columnas++)
             {
-                if (columnas == 0 + auxX || filas == 0 + auxY || columnas == (dimensionx + auxX) - 1 || filas == (dimensiony + auxY) - 1)
+                for (; filas < dimensiony; filas++)
                 {
+                    if (columnas == 0 + auxX || filas == 0 + auxY || columnas == (dimensionx + auxX) - 1 || filas == (dimensiony + auxY) - 1)
+                    {
 
-                    baldosa.layer = 8;
-                    baldosa.GetComponent<BoxCollider2D>().isTrigger = false;
-                    if (columnas == 0 + auxX)
-                    {
-                        baldosa.GetComponent<SpriteRenderer>().sprite = pared;
+                        baldosa.layer = 8;
+                        baldosa.GetComponent<BoxCollider2D>().isTrigger = false;
+                        if (columnas == 0 + auxX)
+                        {
+                            baldosa.GetComponent<SpriteRenderer>().sprite = pared;
 
-                    }
-                    else if (filas == (dimensiony + auxY) - 1 && (columnas != 0 + auxX && columnas != (dimensionx - 1) + auxX))
-                    {
-                        baldosa.GetComponent<SpriteRenderer>().sprite = pared;
-                        sala[columnas, filas + 1] = Instantiate(baldosa, new Vector2(columnas, filas + 1), transform.rotation);
-                        baldosa.GetComponent<SpriteRenderer>().sprite = pared_top;
+                        }
+                        else if (filas == (dimensiony + auxY) - 1 && (columnas != 0 + auxX && columnas != (dimensionx - 1) + auxX))
+                        {
+                            baldosa.GetComponent<SpriteRenderer>().sprite = pared;
+                            sala[columnas, filas + 1] = Instantiate(baldosa, new Vector2(columnas, filas + 1), transform.rotation);
+                            sala[columnas,filas + 1].transform.parent = Rooms[0].transform;
+                            baldosa.GetComponent<SpriteRenderer>().sprite = pared_top;
 
-                    }
-                    else if (filas == 0 + auxY)
-                    {
-                        baldosa.GetComponent<SpriteRenderer>().sprite = pared;
+                        }
+                        else if (filas == 0 + auxY)
+                        {
+                            baldosa.GetComponent<SpriteRenderer>().sprite = pared;
 
-                    }
-                    else if (columnas == (dimensionx + auxX) - 1)
-                    {
-                        baldosa.GetComponent<SpriteRenderer>().sprite = pared;
+                        }
+                        else if (columnas == (dimensionx + auxX) - 1)
+                        {
+                            baldosa.GetComponent<SpriteRenderer>().sprite = pared;
 
-                    }
-                    if (columnas == 0 + auxX && filas == 0 + auxY)
-                    {
-                        baldosa.tag = "esquina inferior izquierda";
-                    }
-                    else if (columnas == dimensionx + auxX - 1 && filas == 0 + auxY)
-                    {
-                        baldosa.tag = "esquina inferior derecha";
-                    }
-                    else if (columnas == 0 + auxX && filas == dimensiony + auxY - 1)
-                    {
-                        baldosa.tag = "esquina superior izquierda";
-                    }
-                    else if (columnas == dimensionx + auxX - 1 && filas == dimensiony + auxY - 1)
-                    {
-                        baldosa.tag = "esquina superior derecha";
+                        }
+                        if (columnas == 0 + auxX && filas == 0 + auxY)
+                        {
+                            baldosa.tag = "esquina inferior izquierda";
+                        }
+                        else if (columnas == dimensionx + auxX - 1 && filas == 0 + auxY)
+                        {
+                            baldosa.tag = "esquina inferior derecha";
+                        }
+                        else if (columnas == 0 + auxX && filas == dimensiony + auxY - 1)
+                        {
+                            baldosa.tag = "esquina superior izquierda";
+                        }
+                        else if (columnas == dimensionx + auxX - 1 && filas == dimensiony + auxY - 1)
+                        {
+                            baldosa.tag = "esquina superior derecha";
+                        }
+                        else
+                        {
+                            baldosa.tag = "muro";
+                        }
+
+
                     }
                     else
                     {
-                        baldosa.tag = "muro";
+
+                        baldosa.GetComponent<SpriteRenderer>().sprite = suelo;
+                        baldosa.tag = "suelo" + tag;
+                        baldosa.layer = 9;
+                        baldosa.GetComponent<BoxCollider2D>().isTrigger = true;
+
                     }
 
-
+                    sala[columnas, filas] = Instantiate(baldosa, new Vector2(columnas, filas), transform.rotation);
+                    sala[columnas,filas].transform.parent = Rooms[0].transform;
                 }
-                else
-                {
-
-                    baldosa.GetComponent<SpriteRenderer>().sprite = suelo;
-                    baldosa.tag = "suelo" + tag;
-                    baldosa.layer = 9;
-                    baldosa.GetComponent<BoxCollider2D>().isTrigger = true;
-
-                }
-
-                sala[columnas, filas] = Instantiate(baldosa, new Vector2(columnas, filas), transform.rotation);
+                filas = 0;
             }
-            filas = 0;
+        
+            rooms[0].GetSala(sala,dimensionx,dimensiony);
+            sala = null;
         }
-        rooms[0].GetSala(sala,dimensionx,dimensiony);
-        sala = null;
-        rooms[0].DestroySala();
 
     }
 
---
+++ proyecto junto/Assets/codigo/mapa/room.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class room : MonoBehaviour {
+    
+	private GameObject [,] sala;
+    private int dimensionX;
+    private int dimensionY;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void GetSala(GameObject[,]sala,int dimensionX, int dimensionY){
+        Debug.Log("he entrado");
+        this.sala = new GameObject[dimensionX,dimensionY+1];
+		//this.sala = sala;
+        this.dimensionX = dimensionX;
+        this.dimensionY = dimensionY;
+        for(int x = 0;x < this.dimensionX; x++){
+            for(int y = 0; y < this.dimensionY; y++){
+                this.sala[x,y] = sala[x,y];
+                Debug.Log(this.sala[x,y]);
+
+            }
+        }
+
+	}
+	public void DestroySala() {
+        Destroy(sala[1,1]);
+        for (int x = 0; x < dimensionX; x ++)
+        {
+            for (int y = 0; y < dimensionY; y++)
+            {
+                Destroy(sala[x, y]);
+
+            }
+        }
+
+    }
+
+}
